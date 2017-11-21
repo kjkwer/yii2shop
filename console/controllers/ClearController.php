0fc4db7b96451010ba4d0d597ce0f2ad06a3fcd4
kjkwer <lilinfeng1024@163.com>
2017-11-21 22:09:10 +0800

    jj

--- console/controllers/ClearController.php
+++ console/controllers/ClearController.php
@@ -13,13 +13,10 @@ class ClearController extends Controller
 {
     //>>设置定时器,自动清理超时未支付订单
     public function actionClearOrder(){
-        while (1){
-            set_time_limit(0);//不限制该脚本执行时间
-            $time = time();
-            $sql = 'update `order` set status=0 WHERE status=1 AND '.$time.'-`create_time`>60';
-            \Yii::$app->db->createCommand($sql)->execute();//执行sql命令
-            sleep(1);  //每隔1秒执行一致
-        }
+        set_time_limit(0);//不限制该脚本执行时间
+        $time = time();
+        $sql = 'update `order` set status=0 WHERE status=1 AND '.$time.'-`create_time`>60';
+        \Yii::$app->db->createCommand($sql)->execute();//执行sql命令
     }
     //>>将redis中的商品数量同步至数据表中
     public function actionToTable(){
